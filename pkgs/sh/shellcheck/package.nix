324a12e5203f79ee5862d4e067214185a5877b78
Jörg Thalheim <joerg@thalheim.io>
2026-09-09 13:44:05 +0200

    shellcheck: allow the aeson and QuickCheck pandoc brought into the hackage set

--- pkgs/sh/shellcheck/package.nix
+++ pkgs/sh/shellcheck/package.nix
@@ -3,4 +3,6 @@ package {
   name = "shellcheck";
   uses = [ "cabal" ];
   cabal.exes = [ "shellcheck" ];
+  # 0.11.0 caps aeson <2.3 and QuickCheck <2.17, the shared hackage set carries pandoc's newer ones
+  cabal.project = "allow-newer: ShellCheck:aeson, ShellCheck:QuickCheck";
 }
