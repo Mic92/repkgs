d9410a1a048bda9da53ba5ce981711e811342fd1
Jörg Thalheim <joerg@thalheim.io>
2026-09-10 22:51:21 +0200

    cmake: 4.1.2 -> 4.4.3

--- pkgs/cm/cmake/package.nix
+++ pkgs/cm/cmake/package.nix
@@ -18,11 +18,10 @@ package (
       {
         # configured by the build machine's cmake (the cmake build system brings it)
         uses = [ "cmake" ];
-        cmake.flags = [
-          "-DCMAKE_USE_OPENSSL=OFF"
-          "-DBUILD_TESTING=OFF"
-          "-DCMAKE_USE_SYSTEM_LIBRARIES=OFF"
-        ];
+        cmake.defs = {
+          CMAKE_USE_OPENSSL = false;
+          CMAKE_USE_SYSTEM_LIBRARIES = false;
+        };
       }
     else
       {
@@ -37,7 +36,7 @@ package (
               cd $c.build
               (x $env.CONFIG_SHELL $"($c.src)/bootstrap" $"--prefix=($c.out)" $"--parallel=($c.njobs)" --no-system-libs
                 --no-qt-gui --docdir=share/doc/cmake --mandir=share/man
-                -- -DCMAKE_USE_OPENSSL=OFF -DBUILD_TESTING=OFF -DCMake_BUILD_LTO=OFF)
+                -- -DCMAKE_USE_OPENSSL=OFF -DBUILD_TESTING=OFF -DCMake_BUILD_LTO=OFF $"-DCMAKE_SYSTEM_PREFIX_PATH=($c.platform.sysroot)")
             '';
           }
           "autotools.build"
