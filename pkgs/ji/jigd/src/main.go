4c96f7e8fa00a26765e37db6ee2edf54508c7d4d
Jörg Thalheim <joerg@thalheim.io>
2026-09-10 10:21:34 +0200

    jigd: socket dir 0755, nix-daemon stats the host path as root when mapping it into the sandbox

--- pkgs/ji/jigd/src/main.go
+++ pkgs/ji/jigd/src/main.go
@@ -170,7 +170,7 @@ func main() {
 		log.Fatal(err)
 	}
 	// a private directory for the socket: connecting to it is trusting whoever serves it
-	if err := os.MkdirAll(filepath.Dir(sock), 0o700); err != nil {
+	if err := os.MkdirAll(filepath.Dir(sock), 0o755); err != nil {
 		log.Fatal(err)
 	}
 	os.Remove(sock)
