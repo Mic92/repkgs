f68f61b531622f9d5daaef61a5b170ee34339f17
Jörg Thalheim <joerg@thalheim.io>
2026-09-10 22:51:17 +0200

    shell.nix: nushell and bsdtar for uptrack

--- shell.nix
+++ shell.nix
@@ -1,7 +1,12 @@
-# Development shell: `treefmt` (treefmt.nix). Not part of the package set. Plain nixpkgs.
+# Development shell: `treefmt` (treefmt.nix) and what uptrack shells out to. Not part of the
+# package set. Plain nixpkgs.
 {
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.mkShellNoCC {
-  packages = [ (import ./treefmt.nix { inherit pkgs; }) ];
+pkgs.mkShell {
+  packages = [
+    (import ./treefmt.nix { inherit pkgs; })
+    pkgs.nushell
+    pkgs.libarchive # bsdtar, uptrack's prefetch unpacks like nix/sources.nix does
+  ];
 }
