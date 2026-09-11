b2b57814832459db7a154ebc52c595984237b612
Jörg Thalheim <joerg@thalheim.io>
2026-09-11 15:25:19 +0200

    jsem: run its test only when tests run (cross: when binfmt makes that transparent)

--- pkgs/js/jsem/package.nix
+++ pkgs/js/jsem/package.nix
@@ -10,8 +10,10 @@ package {
       run = ''
         # the same default jig has: the daemon socket in the state dir next to the store
         let flags = [-std=c++26 -O2 -Wall -Wextra -Werror -UNDEBUG $"-DJSEM_DEFAULT_SOCK=\"($env.NIX_STORE | path dirname)/var/nix/jigd/socket\"" broker.cc]
-        x c++ ...$flags jsem_test.cc -o jsem_test
-        x ./jsem_test
+        if $c.testsRun {
+          x c++ ...$flags jsem_test.cc -o jsem_test
+          x ./jsem_test
+        }
         mkdir $"($c.out)/bin"
         x c++ ...$flags main.cc -o $"($c.out)/bin/jsem"
       '';
