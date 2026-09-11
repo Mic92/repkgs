28061adba6d2856de5706df3d64e43d20b35c68b
Aliaksandr <grubian2@gmail.com>
2026-09-11 15:38:03 +0300

    nix/package: enable content-addressed outputs

--- nix/package.nix
+++ nix/package.nix
@@ -63,6 +63,9 @@ let
   setCommon = {
     inherit (platform) system;
     __structuredAttrs = true;
+    __contentAddressed = true;
+    outputHashMode = "recursive";
+    outputHashAlgo = "sha256";
     builder = "${nu}/bin/nu";
     # platform facts autoconf would otherwise probe (or guess, when cross): nix/config.site
     CONFIG_SITE = "${../nix/config.site}";
