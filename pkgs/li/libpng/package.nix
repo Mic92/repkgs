29c088f6f621f6ee026552720be503a18ccd627a
Jörg Thalheim <joerg@thalheim.io>
2026-09-11 15:54:00 +0200

    libpng: install the .pc files under the condition they are generated with (pnggroup/libpng#924)

--- pkgs/li/libpng/package.nix
+++ pkgs/li/libpng/package.nix
@@ -5,6 +5,8 @@
 package {
   name = "libpng";
   uses = [ "cmake" ];
+  # upstreamable: generated under NOT WIN32, installed under NOT CMAKE_HOST_WIN32
+  patches = [ ./cmake-install-pc-when-generated.patch ];
   cmake.defs = {
     PNG_STATIC = false;
     PNG_TOOLS = true;
