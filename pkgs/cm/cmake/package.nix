50a0ce7b83befab1c7632b0120e3c24d4aa8fb79
Jörg Thalheim <joerg@thalheim.io>
2026-09-12 23:21:51 +0200

    cmake: system cppdap, jsoncpp, librhash, libform

--- pkgs/cm/cmake/package.nix
+++ pkgs/cm/cmake/package.nix
@@ -8,11 +8,15 @@
 let
   system = builtins.filter (p: p.supported) [
     pkgs.bzip2
+    pkgs.cppdap
     pkgs.curl
     pkgs.expat
+    pkgs.jsoncpp
     pkgs.libarchive
     pkgs.libuv
+    pkgs.ncurses
     pkgs.nghttp2
+    pkgs.rhash
     pkgs.xz
     pkgs.zlib
     pkgs.zstd
@@ -31,15 +35,18 @@ variant pkgs.cmake-bootstrap {
       BUILD_TESTING = false;
       CMAKE_USE_OPENSSL = false;
       CMAKE_USE_SYSTEM_LIBRARY_BZIP2 = has pkgs.bzip2;
+      CMAKE_USE_SYSTEM_LIBRARY_CPPDAP = has pkgs.cppdap;
       CMAKE_USE_SYSTEM_LIBRARY_CURL = has pkgs.curl;
       CMAKE_USE_SYSTEM_LIBRARY_EXPAT = has pkgs.expat;
+      CMAKE_USE_SYSTEM_LIBRARY_FORM = has pkgs.ncurses;
+      CMAKE_USE_SYSTEM_LIBRARY_JSONCPP = has pkgs.jsoncpp;
+      CMAKE_USE_SYSTEM_LIBRARY_LIBRHASH = has pkgs.rhash;
       CMAKE_USE_SYSTEM_LIBRARY_LIBARCHIVE = has pkgs.libarchive;
       CMAKE_USE_SYSTEM_LIBRARY_LIBLZMA = has pkgs.xz;
       CMAKE_USE_SYSTEM_LIBRARY_LIBUV = has pkgs.libuv;
       CMAKE_USE_SYSTEM_LIBRARY_NGHTTP2 = has pkgs.nghttp2;
       CMAKE_USE_SYSTEM_LIBRARY_ZLIB = has pkgs.zlib;
       CMAKE_USE_SYSTEM_LIBRARY_ZSTD = has pkgs.zstd;
-      # not packaged: cppdap, form, jsoncpp, librhash stay bundled
     };
   };
   dependencies.set = system;
