1537e5a4c12c152b7f6a8f199d38e4e1313b1577
Jörg Thalheim <joerg@thalheim.io>
2026-09-13 08:40:43 +0200

    gawk: AWKPATH, AWKLIBPATH, locale and awklib helpers relative to the binary

--- pkgs/ga/gawk/package.nix
+++ pkgs/ga/gawk/package.nix
@@ -3,6 +3,8 @@ package {
   name = "gawk";
   uses = [ "autotools" ];
   bootstrapTools = true;
+  # AWKPATH, AWKLIBPATH, locale and awklib helpers relative to the binary
+  patches = [ ./relocatable.patch ];
   autotools.flags = [ "--disable-mpfr" ];
   tests.run = false; # locale-dependent, wants a full /usr/share/locale
   bin = [
