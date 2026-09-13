f41a6fbc8b34d937ef6500d7c0947f2f5e7a10e4
Jörg Thalheim <joerg@thalheim.io>
2026-09-13 10:01:34 +0200

    alsa-lib: alsa.conf and ucm dirs relative to libasound

--- pkgs/al/alsa-lib/package.nix
+++ pkgs/al/alsa-lib/package.nix
@@ -2,8 +2,8 @@
 package {
   name = "alsa-lib";
   uses = [ "autotools" ];
-  # plugins are the system's. alsa.conf ships in $out/share/alsa, found relative to libasound
-  # only if ALSA_CONFIG_DIR says so: applications playing sound set it or use the system's
+  # plugins are the system's
+  patches = [ ./relocatable.patch ];
   autotools.flags = [
     "--with-plugindir=/usr/lib/alsa-lib"
     "--disable-python"
