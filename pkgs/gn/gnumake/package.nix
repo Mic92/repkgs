419ac748ae3f12c80b69a0de598cfac217cd15eb
Jörg Thalheim <joerg@thalheim.io>
2026-09-13 08:40:43 +0200

    gnumake: no prefix include and lib search dirs, locale dir via reloc.h

--- pkgs/gn/gnumake/package.nix
+++ pkgs/gn/gnumake/package.nix
@@ -3,6 +3,8 @@ package {
   name = "gnumake";
   uses = [ "autotools" ];
   bootstrapTools = true;
+  # `include`/-l search and locale dir named the install prefix
+  patches = [ ./relocatable.patch ];
   autotools.flags = [ "--without-guile" ];
   autotools.makeFlags = [ "MAKEINFO=true" ];
   tests.relocated = true;
