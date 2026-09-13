63b3b6d429c8bc1a3ffec847679739d4a2da46e2
Aliaksandr <grubian2@gmail.com>
2026-09-13 17:27:08 +0300

    libgit2: enable openssh

--- pkgs/li/libgit2/package.nix
+++ pkgs/li/libgit2/package.nix
@@ -11,18 +11,20 @@ package {
     BUILD_TESTS = false;
     BUILD_CLI = true;
     REGEX_BACKEND = "pcre2";
-    USE_SSH = "exec";
+    USE_SSH = "libssh2";
   };
   dependencies = [
     pkgs.openssl
     pkgs.pcre2
     pkgs.zlib
+    pkgs.libssh2
   ];
-  # libgit2.pc Requires.private openssl, libpcre2-8, zlib: pkgconf needs them for --cflags too
+  # libgit2.pc Requires.private openssl, libpcre2-8, zlib, libssh2: pkgconf needs them for --cflags too
   exports.propagate = [
     pkgs.openssl
     pkgs.pcre2
     pkgs.zlib
+    pkgs.libssh2
   ];
   buildDependencies = [
     buildPkgs.pkgconf
