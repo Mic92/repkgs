90f941c6dbc7b11bc50bad71f2e7a1bda1f75b7b
Jörg Thalheim <joerg@thalheim.io>
2026-09-13 16:41:14 +0200

    go-bootstrap: source rows keyed by os-cpu, they are linux tarballs

--- pkgs/go/go-bootstrap/package.nix
+++ pkgs/go/go-bootstrap/package.nix
@@ -1,9 +1,11 @@
 # Upstream's static Go toolchain, only ever a buildDependency of pkgs/go/go
 {
   package,
+  platform,
 }:
 package {
   name = "go-bootstrap";
+  source = "${platform.os}-${platform.cpu}";
   # static binaries, nothing to implant: "ldso" just skips debug split and fixup without pulling in formatelf (and thereby rust)
   prebuilt = "ldso";
   install."." = [
