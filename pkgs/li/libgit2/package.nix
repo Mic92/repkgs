acf928762911b2de0d3a0f8212d3c7b29c56a424
Aliaksandr <grubian2@gmail.com>
2026-09-13 17:27:08 +0300

    libgit2: enable openssh

--- pkgs/li/libgit2/package.nix
+++ pkgs/li/libgit2/package.nix
@@ -7,25 +7,17 @@ package {
   name = "libgit2";
   uses = [ "cmake" ];
   cmake.defs = {
-    CMAKE_C_STANDARD = "23"; # silences -Wc11-extensions build log spam
     BUILD_TESTS = false;
-    BUILD_CLI = true;
+    BUILD_CLI = false;
     REGEX_BACKEND = "pcre2";
-    USE_SSH = "exec";
+    USE_SSH = "libssh2";
   };
   dependencies = [
     pkgs.openssl
     pkgs.pcre2
     pkgs.zlib
+    pkgs.libssh2
   ];
-  # libgit2.pc Requires.private openssl, libpcre2-8, zlib: pkgconf needs them for --cflags too
-  exports.propagate = [
-    pkgs.openssl
-    pkgs.pcre2
-    pkgs.zlib
-  ];
-  buildDependencies = [
-    buildPkgs.pkgconf
-  ];
-  tests.run = false; # slow and require network
+  buildDependencies = [ buildPkgs.pkgconf ];
+  tests.run = false; # slow and wants the network
 }
