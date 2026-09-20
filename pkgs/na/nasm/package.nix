987edc7a1f2ae59c469693b19bdf161b7096b0a0
Jörg Thalheim <joerg@thalheim.io>
2026-09-20 09:27:40 +0200

    nasm: build in tree on windows
    The manifest rule names win/manifest.rc relative to the build directory.

--- pkgs/na/nasm/package.nix
+++ pkgs/na/nasm/package.nix
@@ -1,7 +1,14 @@
-{ package, buildPkgs }:
+{
+  package,
+  buildPkgs,
+  platform,
+  on,
+}:
 package {
   name = "nasm";
   uses = [ "autotools" ];
+  # windows: win/manifest.rc is named relative to the build dir, and win/ is not created there
+  autotools.outOfTree = platform.os != "windows";
   buildDependencies = [ buildPkgs.perl ];
   tests.run = false; # no check target, test/ compares against a reference nasm
 }
